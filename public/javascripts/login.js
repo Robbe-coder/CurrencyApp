786475a162da8c7c82117c33a43c923251353a99
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-05-04 16:02:08 +0200

    feat: possible to login with fetch

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,1 +1,30 @@
-console.log("let's go");
+const signupBtn = document.querySelector(".button--submit");
+
+signupBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let username = document.querySelector('#username').value;
+    let password = document.querySelector('#password').value;
+
+
+    
+        fetch('http://localhost:3000/users/login',{
+            method: "post",
+            headers:{
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            })
+        }).then(response => {
+            return response.json();
+
+        }).then(json => {
+            if(json.status === "success"){
+                console.log("login succes")
+                
+            }
+        })
+    
+});
