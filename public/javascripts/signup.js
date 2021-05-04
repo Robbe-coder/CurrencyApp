f33c41594ffc07df3425e37db983b586a1500a99
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-05-04 14:59:45 +0200

    feat: signup can be done through front-end

---
+++ public/javascripts/signup.js
@@ -0,0 +1,33 @@
+const signupBtn = document.querySelector(".button--submit");
+
+signupBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let username = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+    let passwordRepeat = document.querySelector("#password-repeat").value;
+
+    if(password === passwordRepeat){
+        fetch('http://localhost:3000/users/signup',{
+            method: "post",
+            headers:{
+                'Content-Type': 'application/json'
+            },
+
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            })
+        }).then(response => {
+            return response.json();
+
+        }).then(json => {
+            if(json.status === "succes"){
+                console.log("signup succes")
+            }
+        })
+    }
+    else{
+        console.log(`passwords don't match`)
+    }
+    
+});
