dbb60910cc4d95d2f8d161b30c9816e309adc185
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-05-05 08:59:18 +0200

    feat: added watch for scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,17 @@
-function test(){
-    console.log("ja");
+const { src, dest } = require('gulp');
+const { watch, series } = require('gulp');
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+sass2css = function(){
+    return src('./public/scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest("./public/css"));
 }
-exports.test = test;
+exports.default = function(){
+
+    watch('./public/scss/**/*.scss', sass2css);
+    
+}
