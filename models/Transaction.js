bf1ca2ae7790c20e532cf5f5abc093badf12abbd
Bob Storms <bob.storms@hotmail.be>
2021-05-12 11:15:56 +0200

    feat: add model for transaction

---
+++ models/Transaction.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Transaction = new Schema({
+    amount: Number,
+    message: String,
+    person_to_id: Number,
+    person_from_id: Number,
+    reason: String
+});
