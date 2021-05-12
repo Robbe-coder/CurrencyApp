6ae7346aff19a293e80842539908fb3f5012838d
Bob Storms <bob.storms@hotmail.be>
2021-05-12 11:27:44 +0200

    refactor: make code prettier

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,16 +1,14 @@
 const signupBtn = document.querySelector(".button--submit");
 
-signupBtn.addEventListener("click",(e)=>{
+signupBtn.addEventListener("click", (e) => {
     e.preventDefault();
     let username = document.querySelector('#username').value;
     let password = document.querySelector('#password').value;
-
-
     
-        fetch('http://localhost:3000/users/login',{
+        fetch("http://localhost:3000/users/login", {
             method: "post",
-            headers:{
-                'Content-Type': 'application/json'
+            headers: {
+                "Content-Type": "application/json"
             },
 
             body: JSON.stringify({
--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,16 +1,16 @@
 const signupBtn = document.querySelector(".button--submit");
 
-signupBtn.addEventListener("click",(e)=>{
+signupBtn.addEventListener("click", (e) => {
     e.preventDefault();
     let username = document.querySelector('#email').value;
     let password = document.querySelector('#password').value;
     let passwordRepeat = document.querySelector("#password-repeat").value;
 
-    if(password === passwordRepeat){
-        fetch('http://localhost:3000/users/signup',{ // Variabele toevoegen
+    if(password === passwordRepeat) {
+        fetch("http://localhost:3000/users/signup", { // Variabele toevoegen
             method: "post",
             headers:{
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json"
             },
 
             body: JSON.stringify({
@@ -21,17 +21,17 @@ signupBtn.addEventListener("click",(e)=>{
             return response.json();
 
         }).then(json => {
-            if(json.status === "succes"){
-                console.log("signup succes")
-                document.querySelector('#name').value="";
-                document.querySelector('#email').value ="";
-                document.querySelector('#password').value ="";
-                document.querySelector('#password-repeat').value ="";
+            if(json.status === "succes") {
+                console.log("signup succes");
+                document.querySelector('#name').value = "";
+                document.querySelector('#email').value = "";
+                document.querySelector('#password').value = "";
+                document.querySelector('#password-repeat').value = "";
             }
-        })
+        });
     }
-    else{
-        console.log(`passwords don't match`)
+    else {
+        console.log("Passwords don't match.");
     }
     
 });
