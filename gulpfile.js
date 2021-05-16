c9c9c7bc267fe91947051d8f12f0e373b9277254
Bob Storms <bob.storms@hotmail.be>
2021-05-16 12:50:48 +0200

    fix: moved scss to src folder

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,20 @@ const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
 sass2css = function(){
-    return src('./public/scss/style.scss')
+    return src('./src/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest("./public/css"));
 }
 
-  
 minify = function(){
     return gulp.src('./src/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('./public'));
 }
+
 exports.default = function(){
 
-    watch('./public/scss/**/*.scss', sass2css);
+    watch('./src/scss/**/*.scss', sass2css);
     watch("./src/**/*.html", minify);
     
 }
