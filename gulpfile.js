7808219c1564115d8eb3fc213bee19a337053587
Bob Storms <bob.storms@hotmail.be>
2021-05-16 13:11:29 +0200

    chore: add gulp-babel and gulp-babel-minify to compile and minify js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,42 @@
 const { src, dest } = require('gulp');
 const { watch, series } = require('gulp');
-const htmlmin = require('gulp-htmlmin');
 
+const htmlmin = require('gulp-htmlmin');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
+const babel = require('gulp-babel');
+const minify = require('gulp-babel-minify');
 
-sass2css = function(){
+sass2css = function() {
     return src('./src/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest("./public/css"));
 }
 
-minify = function(){
+minifyHtml = function() {
     return gulp.src('./src/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('./public'));
 }
 
-exports.default = function(){
+gulpBabelMinify = function() { 
+    return gulp.src('./src/js/*.js')
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(minify({
+        mangle: {
+            keepClassName: true
+        }
+    }))
+    .pipe(gulp.dest('./public/js'))
+};
+
+exports.default = function() {
 
     watch('./src/scss/**/*.scss', sass2css);
-    watch("./src/**/*.html", minify);
+    watch('./src/**/*.html', minifyHtml);
+    watch('./src/js/**/*.js', gulpBabelMinify);
     
 }
