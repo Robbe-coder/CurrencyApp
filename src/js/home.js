7645907b016b09089446f75b7c5e4ccaaf5926d0
Robbe Bierebeeck <77198548+Robbe-coder@users.noreply.github.com>
2021-05-30 14:58:17 +0200

    fix: people with no transactions don't get an error message

--- src/js/home.js
+++ src/js/home.js
@@ -13,38 +13,42 @@ window.addEventListener("load",()=>{
         console.log(json);
         //console.log(json.transactions[0].amount);
         console.log(json.transactions.length);
-        let arrayLength= json.transactions.length;
-        for(let i = arrayLength -1; i >= arrayLength -3;i--){
-            
-            let transactionElement = document.querySelector(".transactionslist");
-            let card = document.createElement("div");
-            card.classList.add("transaction__card");
-            let imageCard = document.createElement("img");
-            imageCard.classList.add("icon");
-            imageCard.src ="images/credit-card.svg";
-            let text = document.createElement("p");
-            text.classList.add("transaction__text");
-            let span = document.createElement("span");
-            let imageArrow = document.createElement("img");
-            span.classList.add("transaction__text--highlight");
-            imageArrow.classList.add("icon");
-            json.transactions[i];
-           if(json.personid === json.transactions[i].person_to_id){
+        if(json.transactions.length !== 0){
+
+            let arrayLength= json.transactions.length;
+            for(let i = arrayLength -1; i >= arrayLength -3;i--){
                 
-                text.innerHTML = ` received ${json.transactions[i].amount} coins`;
-                imageArrow.src="images/arrow-left.svg"
+                let transactionElement = document.querySelector(".transactionslist");
+                let card = document.createElement("div");
+                card.classList.add("transaction__card");
+                let imageCard = document.createElement("img");
+                imageCard.classList.add("icon");
+                imageCard.src ="images/credit-card.svg";
+                let text = document.createElement("p");
+                text.classList.add("transaction__text");
+                let span = document.createElement("span");
+                let imageArrow = document.createElement("img");
+                span.classList.add("transaction__text--highlight");
+                imageArrow.classList.add("icon");
+                json.transactions[i];
+                
+               if(json.personid === json.transactions[i].person_to_id){
+                    
+                    text.innerHTML = ` received ${json.transactions[i].amount} coins`;
+                    imageArrow.src="images/arrow-left.svg"
+                }
+                else{
+                    text.innerHTML = ` send ${json.transactions[i].amount} coins`;
+                    imageArrow.src="images/arrow-right.svg"
+                }
+               text.appendChild(span);
+                card.appendChild(imageCard);
+                card.appendChild(text);
+                card.appendChild(imageArrow);
+                transactionElement.appendChild(card);
+                //document.querySelector(".transaction__text").innerHTML = `${json.transactions[i].amount} + ${json.transactions[i].message}`;
+                
             }
-            else{
-                text.innerHTML = ` send ${json.transactions[i].amount} coins`;
-                imageArrow.src="images/arrow-right.svg"
-            }
-           text.appendChild(span);
-            card.appendChild(imageCard);
-            card.appendChild(text);
-            card.appendChild(imageArrow);
-            transactionElement.appendChild(card);
-            //document.querySelector(".transaction__text").innerHTML = `${json.transactions[i].amount} + ${json.transactions[i].message}`;
-            
         }
 
     }).catch(error => {
