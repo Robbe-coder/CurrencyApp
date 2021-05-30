0c8e08249328480dd41d0065da97d916a7855dd7
Bob Storms <bob.storms@hotmail.be>
2021-05-30 19:24:07 +0200

    fix: deleted view engine setup in app.js

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ mongoose.set('useCreateIndex', true);
 mongoose.connect(process.env.dbconn || config.get('Database.conn'), {useNewUrlParser: true, useUnifiedTopology: true});
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
