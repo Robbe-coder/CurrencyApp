0ef2d3b158319f53c009e438aff3c4707322ac5c
Bob Storms <bob.storms@hotmail.be>
2021-05-30 13:33:27 +0200

    refactor: delete comments in js of newtransaction

--- src/js/newtransaction.js
+++ src/js/newtransaction.js
@@ -3,6 +3,8 @@ let names = [];
 const inputUsername = document.getElementById("name");
 console.log(inputUsername);
 
+// Code inspired by: https://www.w3schools.com/howto/howto_js_autocomplete.asp
+
 inputUsername.addEventListener("keyup", function (e) {
 
     fetch(settings.host + "/api/users?username=" + e.target.value, {
@@ -13,15 +15,12 @@ inputUsername.addEventListener("keyup", function (e) {
     }).then(response => {
         return response.json();
     }).then(json => {
-        /*close any already open lists of autocompleted values*/
         closeAllLists();
 
-        /*create a DIV element that will contain the items (values):*/
         let allValues = document.createElement("div");
         allValues.setAttribute("id", this.id + "autocomplete-list");
         allValues.setAttribute("class", "form__autocomplete-items");
 
-        /*append the DIV element as a child of the autocomplete container:*/
         this.parentNode.appendChild(allValues);
 
         let users = json.user;
@@ -29,18 +28,11 @@ inputUsername.addEventListener("keyup", function (e) {
             for(var i = 0; i < users.length; i++) {
                 let username = users[i].username;
 
-                /*create a DIV element for each matching element:*/
                 let newValue = document.createElement("div");
-                /*make the matching letters bold:*/
                 newValue.innerHTML = username;
-                /*insert a input field that will hold the current array item's value:*/
                 newValue.innerHTML += "<input type='hidden' value='" + username + "'>";
-                /*execute a function when someone clicks on the item value (DIV element):*/
                 newValue.addEventListener("click", function (e) {
-                    /*insert the value for the autocomplete text field:*/
                     inputUsername.value = this.getElementsByTagName("input")[0].value;
-                    /*close the list of autocompleted values,
-                    (or any other open lists of autocompleted values:*/
                     closeAllLists();
                 });
                 allValues.appendChild(newValue);
@@ -53,8 +45,6 @@ inputUsername.addEventListener("keyup", function (e) {
 });
 
 function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
     var x = document.getElementsByClassName("form__autocomplete-items");
     for (var i = 0; i < x.length; i++) {
         if (elmnt != x[i] && elmnt != inputUsername) {
@@ -63,7 +53,6 @@ function closeAllLists(elmnt) {
     }
 }
 
-/*execute a function when someone clicks in the document:*/
 document.addEventListener("click", function (e) {
     closeAllLists(e.target);
 });
